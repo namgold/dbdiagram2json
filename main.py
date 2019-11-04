42cf442ff5907a534ba13154d7ea32ef8b0cf171
Nam Nguyen <hnamnguyencse@gmail.com>
2019-11-04 21:04:18 +0700

    Use json lib

--- main.py
+++ main.py
@@ -1,79 +1,89 @@
-import re
-import time
+import re, time, json
 
 regexTables = r"""[Tt]able[ ]+(?P<TableName>[^\s]+)(?:[ ]+as[ ]+(?P<TableShortName>[^\s]+))?[ ]*(?:\[(?P<TableOption>.*?)\])?[ ]*\{(?P<DeclBody>.*?)\}"""
-regexFields = r"""(?P<FieldName>[^\s]+)[ ]+(?P<FieldType>[^\s]+)(?:[ ]+\[(?P<FieldOption>.*?)\])?[ ]*\n"""
-regexOption = r"\b(?=\w)[^,]+"
-tableArrayPrefix = "\t"
-tableElementPrefix = "\t\t"
-tablePropsPrefix = "\t\t\t"
-fieldElementPrefix = "\t\t\t\t"
-fieldPropsPrefix = "\t\t\t\t\t"
-fieldOptionPrefix = "\t\t\t\t\t\t"
+regexFields = r"""(?P<FieldName>[^\s]+)[ ]+(?P<FieldType>[^\s]+)(?:[ ]*\[(?P<FieldOption>.*?)\])?[ ]*\n"""
 inputPath = "./dbdiagram.input"
 outputPath = "./dbdiagram.json"
 
-def renderOption(optionString, basePrefix):
+def buildFieldOption(optionString):
     if optionString == None:
-        return "null"
-    result = "["
-    matches = re.finditer(regexOption, optionString)
-    for pi, i in enumerate(matches):
-        if pi > 0:
-            result += ','
-        result += '\n'+ basePrefix + '"' + i.group() + '"'
-    result += '\n' + basePrefix[:-1] + "]"
-    return result
+        return None
+    options = {
+    #    "ref":         None,
+    #    "default" :    None,
+    #    "increment":   None,
+    #    "nullable":    None,
+    #    "primary key": None,
+    #    "unique":      None,
+    #    "note":        None
+    }
+    optionString = optionString.replace('\n', ' ').strip().split(',')
+    for i in optionString:
+        words = i.strip().split(' ')
+        if words[0] == "default:":
+            default = re.findall("(?:\"([^\"]*)\")|(?:'([^']*)')", i)[0]
+            options["default"] = default[0] if default[1] == "" else default[1]
+        elif words[0] == "ref:":
+            matchRef = re.findall("([<>-])[ ]+([^.]+)\.([^,\]]+)", i)[0]
+            options["ref"] = {
+                "op": matchRef[0],
+                "table": matchRef[1],
+                "column": matchRef[2]
+            }
+        elif words[0] == "note:":
+            note = re.findall("(?:\"([^\"]*)\")|(?:'([^']*)')", i)[0]
+            options["note"] = note[0] if note[1] == "" else note[1]
+        elif words[0] == "increment":
+            options[words[0]] = True
+        elif words[0] == "null":
+            options["nullable"] = True
+        elif words[0] == "not":
+            options["nullable"] = False
+        elif words[0] == "primary" or words[0] == "pk":
+            options["primary key"] = True
+        elif words[0] == "unique":
+            options["unique"] = True
+    return options
 
-def renderFields(fieldsString):
-    result = ""
+def buildFields(fieldsString):
+    fields = []
     matchesField = re.finditer(regexFields, fieldsString)
     for pi, i in enumerate(matchesField):
-        if pi > 0:
-            result += ",\n"
-        result += fieldElementPrefix + "{\n"
         group = i.groups()
-        result += fieldPropsPrefix + '"name": "{0}",\n'.format(group[0])
-        result += fieldPropsPrefix + '"type": "{0}",\n'.format(group[1])
-        result += fieldPropsPrefix + \
-            '"options": {0}\n'.format(
-                renderOption(group[2], fieldOptionPrefix))
-        result += fieldElementPrefix + "}"
-    return result
+        field = {
+            "name": group[0],
+            "type": group[1],
+            "options": buildFieldOption(group[2])
+        }
+        fields += [field]
+    return fields
 
-def renderTables(tableString):
-    result = ""
-    matches = re.finditer(regexTables, tableString,
-                              re.MULTILINE | re.VERBOSE | re.DOTALL)
-    result = ""
-    result += '{\n'
-    result += tableArrayPrefix + '"Table":[\n'
+def buildTableOption(tableOptionString):
+    return None
+
+def buildTables(tableString):
+    result = {"Table": []}
+    matches = re.finditer(regexTables, tableString, re.MULTILINE | re.VERBOSE | re.DOTALL)
     for matchNum, match in enumerate(matches, start=1):
-        if matchNum > 1:
-            result += ",\n"
-        result += tableElementPrefix + "{\n"
         group = match.groups()
-        result += tablePropsPrefix + '"name": "{0}",\n'.format(group[0])
-        result += tablePropsPrefix + '"shortName": "{0}",\n'.format(group[1])
-        result += tablePropsPrefix + \
-            '"option": {0},\n'.format(renderOption(group[2], fieldElementPrefix))
-        result += tablePropsPrefix + \
-            '"fields": [\n{0}\n'.format(renderFields(group[3]))
-        result += tablePropsPrefix + "]\n"
-        result += tableElementPrefix + "}"
-        print('.', end='')
-    result += "\n" + tableArrayPrefix + "]\n"
-    result += "}"
-    return (result, matchNum)
+        table = {
+            "name": group[0],
+            "shortName": group[1],
+            "option": buildTableOption(group[2]),
+            "fields": buildFields(group[3])
+        }
+        result["Table"] += [table]
+    return result
 
 print("Generating")
 now = time.time()
 
-dbdiagraminput = open(inputPath, 'r').read()
+dbdiagraminput = open(inputPath, 'r').read().replace('"',"'")
 
-tableOutput = renderTables(dbdiagraminput)
+tableObject = buildTables(dbdiagraminput)
+tableOutput = json.dumps(tableObject, indent=4)
 
-open(outputPath, 'w').write(tableOutput[0])
+open(outputPath, 'w').write(tableOutput)
 
-print("\nGenerate succeeded", tableOutput[1],
+print("\nGenerate succeeded", len(tableObject["Table"]),
       "tables in {0:0.4f} second(s)".format(time.time()-now))
